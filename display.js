178dfb0142f8fb02403880c470958972c0475df7
Luka Uršič <luka.urs@icloud.com>
2023-03-26 10:15:54 +0200

    flags and mines demo

--- display.js
+++ display.js
@@ -1,14 +1,16 @@
 function createGrid(size) {
     let table = document.getElementById('grid')
     for (let i = 0; i < size; i++) {
-        let t_row= document.createElement('tr')
+        let t_row = document.createElement('tr')
 
         for (let j = 0; j < size; j++) {
-            let t_data= document.createElement('td')
+            let t_data = document.createElement('td')
             let button = document.createElement('button')
             button.classList.add("field_button")
-            // button.classList.add("field_button_flagged")
-            // button.classList.add("field_button_mine")
+            if (i + 3 < j)
+                button.classList.add("field_button_flagged")
+            if (i == j)
+                button.classList.add("field_button_mine")
             t_data.appendChild(button)
             t_row.appendChild(t_data)
         }
