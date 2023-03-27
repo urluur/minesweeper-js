42c65ef20beca53a5dff07746d4dbee42a9f47ad
Luka Uršič <luka.urs@icloud.com>
2023-03-27 16:42:52 +0200

    kind of works

--- display.js
+++ display.js
@@ -1,30 +1,67 @@
-let difficulty = {
+let difficulty_presets = {
     easy: [9, 9, 10],
     intermediate: [16, 16, 40],
     expert: [16, 30, 99]
 }
 
-function createGrid(dif) {
-    let table = document.getElementById('grid')
-    for (let i = 0; i < dif[0]; i++) {
-        let t_row = document.createElement('tr')
+let difficulty = difficulty_presets.easy;
 
-        for (let j = 0; j < dif[1]; j++) {
+function displayGrid() {
+    let table = document.getElementById('grid')
+    if (table.hasChildNodes) {
+        table.innerHTML = "";
+    }
+    for (let i = 0; i < difficulty[0]; i++) {
+        let t_row = document.createElement('tr')
+        for (let j = 0; j < difficulty[1]; j++) {
             let t_data = document.createElement('td')
             let button = document.createElement('button')
+            
             button.classList.add("field_button")
-            if (i + 3 < j)
+
+            button.setAttribute("onclick", "clickButton(" + i + ", " + j + ")")
+            button.setAttribute("oncontextmenu", "flagButton(" + i + ", " + j + "); return false;")
+
+            if (grid[i][j].isFlagged) {
                 button.classList.add("field_button_flagged")
-            if (i == j)
+            }
+            if (grid[i][j].isBomb && grid[i][j].isClicked) {
                 button.classList.add("field_button_mine")
+                // reveal all bombs
+                for (let k = 0; k < difficulty[0]; k++) {
+                    for (let l = 0; l < difficulty[1]; l++) {
+                        if (grid[k][l].isBomb) {
+                            grid[k][l].isClicked = true
+                        }
+                    }
+                }
+                // end game
+            }
+            if (grid[i][j].isClicked) {
+                if (grid[i][j].isBomb) {
+                    button.classList.add("field_button")
+                    button.classList.add("field_button_mine")
+                }
+                else if (grid[i][j].neighborBombs == 0) {
+                    button.classList.remove("field_button")
+                    button.classList.add("field_button_empty")
+
+                }
+                if (grid[i][j].neighborBombs > 0) {
+                    button.classList.remove("field_button")
+                    // add picture to button
+                    let img = document.createElement('img')
+                    img.src = "img/num_of_neighbors/open" + grid[i][j].neighborBombs + ".png"
+                    button.classList.add("field_button_img")
+                    button.appendChild(img)
+                }
+            }
             t_data.appendChild(button)
             t_row.appendChild(t_data)
         }
         table.appendChild(t_row)
     }
 }
-
-createGrid(difficulty.easy)
 
 function closeWindow() {
     let window = document.getElementById("game_window")
@@ -35,5 +72,6 @@ function closeWindow() {
         document.getElementById("start").src="img/taskbar_left_inactive.png";
     } else {
         document.getElementById("start").src="img/taskbar_left_active.png";
+        startGame();
     }
 }
